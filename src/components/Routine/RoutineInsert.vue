55fc29480f2dea7025f9bbfa2aeebaadb13f8fe0
dahee <rudtjd98@naver.com>
2022-05-12 17:41:37 +0900

    RoutineUpdate

--- src/components/Routine/RoutineInsert.vue
+++ src/components/Routine/RoutineInsert.vue
@@ -1,16 +1,28 @@
 <template>
     <div>
         <h3>루틴 생성 페이지</h3>
-        
+        <div v-for="(routine,i) in state.routines" :key="i">
+            <input type="text" v-model="state.routines[i]">
+        </div>
+
     </div>
 </template>
 
 <script>
+import { reactive } from 'vue';
 export default {
     setup () {
+        const state = reactive({
+            token : sessionStorage.getItem("TOKEN"),
+            routines : [
+                {rtnday : '월', rtncnt : 1, rtnset : 1, rtnname : '루틴명'},
+                {rtnday : '화', rtncnt : 1, rtnset : 1, rtnname : '루틴명'},
+                {rtnday : '수', rtncnt : 1, rtnset : 1, rtnname : '루틴명'}
+            ],
+        })
         
 
-        return {}
+        return {state}
     }
 }
 </script>
