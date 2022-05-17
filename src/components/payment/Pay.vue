9cc534ebea0f19d36771a47337ed1792b4a71f6b
daheeka <rudtjd98@naver.com>
2022-05-17 22:57:59 +0900

    payUpdate

---
+++ src/components/payment/Pay.vue
@@ -0,0 +1,85 @@
+<template>
+    <div style="padding : 50px">
+        <div v-if="this.pjoinchg">
+        <input type="number" placeholder="결제금액" v-model="this.price" >
+        <el-button type="info" plain size="mini" @click="requestPay">참가비 결제하기</el-button>
+        </div>
+    </div>
+</template>
+
+<script>
+// import axios from 'axios';
+const { IMP } = window;
+export default {
+    created(){
+        this.handleJoinCHG();
+    },
+
+    data(){
+        return{
+            jno : 65,
+            token : sessionStorage.getItem("TOKEN"),
+            pjoinchg : [],
+            price: 0
+        }
+    },
+    methods: {
+
+        // 참여 챌린지 조회 (번호 필요)
+        // 데이터가 안 옴 확인 필요 
+        async handleJoinCHG(){
+            const url =`/ROOT/api/join/selectone?jno=${this.jno}`;
+            const headers = {"Content-Type":"application/json", "token":this.token};
+            const response = await this.axios.get(url, {headers:headers});
+            console.log(response.data)
+            if(response.data.status === 200){
+                this.joinchg = response.data.result;
+            }
+        },
+
+        // 결제
+        requestPay: function () {
+            // IMP.request_pay(param, callback) 결제창 호출
+            IMP.init('imp20187774'); // 발급받은 가맹점 식별코드 사용
+            IMP.request_pay({ // param
+                pg: "html5_inicis", // PG사
+                pay_method: "card", // 결제수단
+                merchant_uid: "CHG_"+new Date().getTime(), // 결제번호
+                name: "챌린지 참가비", // 결제 시 표시되는 이름
+                amount: this.price, // 결제금액
+                buyer_email: "gildong@gmail.com",  // 참여자 이메일
+                buyer_name: "홍길동", // 참여자 이름
+            }, rsp => { // callback
+            if (rsp.success) {// 결제 성공 시 로직
+                alert("결제가 완료되었습니다.");
+                console.log("결제 성공");
+                location.href = "http://localhost:8080/?#/"; // 결제 완료 시 이동할 페이지
+            // axios로 HTTP 요청
+            // axios({
+            //     url: `/ROOT/api/pay/insert.json`, // 서버의 결제 정보
+            //     method: "post",
+            //     headers: { "Content-Type": "application/json" },
+            //     data: {
+            //         impuid: rsp.imp_uid,
+            //         merchantuid: rsp.merchant_uid,
+            //         price : rsp.amount
+            //         join : this.pjoinchg.
+            //         pmethod : rsp.pay_method
+            //     }
+            // }).then((data) => {
+            //     // 서버 결제 API 성공시 로직
+            // })           
+            } else {// 결제 실패 시 로직
+              console.log("결제 실패");
+              alert("결제에 실패하였습니다. 에러 내용: " +  rsp.error_msg);
+            
+            }
+          });
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
