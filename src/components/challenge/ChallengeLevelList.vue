f2a8842d36c6ed1972be4ef2c1666e9b2dfe4d97
akaksk <kangs6n@gmail.com>
2022-05-18 17:47:22 +0900

    리스트 작업

--- src/components/challenge/ChallengeLevelList.vue
+++ src/components/challenge/ChallengeLevelList.vue
@@ -1,15 +1,84 @@
 <template>
     <div>
-        <h3>challenge level list</h3>
+        <div align="center" v-if="state.items">
+            <h3>챌린지 난이도순 목록</h3>
+
+            <table border="1">
+                <tr>
+                    <th>난이도순</th>
+                    <th>제목</th>
+                    <th>소개</th>
+                    <th>난이도</th>
+                    <th>작성일</th>
+                </tr>
+                <tr v-for="tmp in state.items" :key="tmp">
+                    <td>{{tmp.chglevel}}단계</td>
+                    <td @click="handleSelectOne(tmp.chgno)" style="cursor:pointer">{{tmp.chgtitle}}</td>
+                    <td>{{tmp.chgintro}}</td>
+                    <td>좋아요{{tmp.chglike}}개</td>
+                    <td>{{tmp.chgregdate}}</td>
+                </tr>
+            </table>
+
+            <input type="text" v-model="state.text">
+            <button @click="handleData(state.page, state.text)">검색</button>
+
+            <div>
+                <label v-for="tmp in state.pages" :key="tmp">
+                    <button @click="handlePage(tmp, state.text)" >{{ tmp }}</button>
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { onMounted, reactive } from 'vue';
+import axios from 'axios';
+import {useRouter} from 'vue-router';
+
 export default {
     setup () {
-        
+        const router = useRouter();
 
-        return {}
+        const state = reactive({
+            page : 1,
+            pages : 1,
+            text : "",
+        });
+
+        const  handleSelectOne = async(chgno) => {
+            console.log(chgno);
+            router.push({name:"ChallengeOne", query:{chgno:chgno}});
+        }
+
+        // 내가 참가한 첼린지 리스트
+        const handleData = async(tmp, text) => {
+            console.log("page", state.page);
+            console.log("text", state.text);
+            const url = `ROOT/api/challenge/levelselectlist?page=${tmp}&title=${text}`;
+            const headers = {
+                "Content-Type":"application/json"};
+            const response = await axios.get(url, {headers});
+            // console.log(response.data);
+
+            if (response.data.status === 200) {
+                state.items = response.data.result
+                state.pages = response.data.pages
+            }
+
+        };
+
+        const handlePage = async(tmp, text)=> {
+
+            handleData(tmp, text);
+        }
+
+        onMounted(() => {
+            handleData(state.page, state.text);
+        });
+
+        return {state, handleData, handlePage, handleSelectOne}
     }
 }
 </script>
