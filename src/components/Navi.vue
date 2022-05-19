5c3286a41d83c36abc45460a14a4f50537419a7b
kimminju <blesty5683@naver.com>
2022-05-19 23:35:48 +0900

    main front css
    프런트 메인 화면

--- src/components/Navi.vue
+++ src/components/Navi.vue
@@ -3,30 +3,45 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
-                <router-link to="/" class="navbar-brand">헬린지</router-link>
+                <router-link to="/" class="navbar-brand fonts" style="font-size:2.5rem" >헬린지</router-link>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
-                        <li class="nav-item"><router-link to="/" class="nav-link active" aria-current="page">Home</router-link></li>
+                        <!-- <li class="nav-item"><router-link to="/" class="nav-link active" aria-current="page">Home</router-link></li> -->
                         <li class="nav-item" v-if="logged === false"><router-link to="/memberjoin" class="nav-link">회원가입</router-link></li>
                         <li class="nav-item" v-if="logged === false"><router-link to="/login" class="nav-link">로그인</router-link></li>
-                        <li class="nav-item"><router-link to="/board" class="nav-link">자유게시판</router-link></li>
+                        <li class="nav-item"><router-link to="/" class="nav-link">첼린지</router-link></li>
+                        <li class="nav-item"><router-link to="/confirmlist" class="nav-link">인증</router-link></li>
+                        <li class="nav-item"><router-link to="/board" class="nav-link">커뮤니티</router-link></li>
                         <li class="nav-item"><router-link to="/pose" class="nav-link">자세</router-link></li>
-                        <li class="nav-item" v-if="logged === true"><router-link to="/menu1" class="nav-link">마이페이지</router-link></li>
-                        <li class="nav-item" v-if="logged === true"><router-link to="/logout" class="nav-link">로그아웃</router-link></li>
+                        <!-- <li class="nav-item" v-if="logged === true"><router-link to="/menu1" class="nav-link">마이페이지</router-link></li> -->
+                        <!-- <li class="nav-item" v-if="logged === true"><router-link to="/logout" class="nav-link">로그아웃</router-link></li> -->
 
                     </ul>
+
+
+
+                    <!-- 로그인한 사용자의 이메일 -->
                     <div v-if="state.item" style="margin-right:10px">
                         <div class="d-flex" v-if="logged === true">{{state.item.mid}}</div>
                     </div>
-
 
                     <!-- 내리는 메뉴 -->
                     <!-- class="nav-link dropdown-toggle" -->
                     <div class="d-flex" v-if="logged === true">
                         <li class="nav-item dropdown d-flex">
-                            <a  id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><img :src="state.image" style="width:50px;"/></a>
+
+                            <!-- 프로필 이미지 -->
+                            <a  id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <div style="border-radius: 70%; width:50px; height:50px;overflow: hidden; border: 1px solid red;">
+                                    <img :src="state.image" style="width:100%; height:100% object-fit: cover;" />
+                                </div>
+                            </a>
                             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li>
+                                    <router-link to="/logout" class="dropdown-item">로그아웃</router-link>
+                                    <hr class="dropdown-divider" />
+                                </li>
                                 <li><a class="dropdown-item" href="#/menu1">정보수정</a></li>
                                 <li><hr class="dropdown-divider" /></li>
                                 <li><a class="dropdown-item" href="#/menu3">1:1 문의</a></li>
@@ -131,5 +146,8 @@ export default {
 
 <style lang="css" scoped>
 
+.fonts {
+    font-family: 'Do Hyeon', sans-serif;
+}
 
 </style>
