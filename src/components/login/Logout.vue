d255420ad04a2d8fd5d23ae072cbf2d7cc186e47
alsendrha <alsendrha@naver.com>
2022-05-29 16:41:09 +0900

    34343
    34

--- src/components/login/Logout.vue
+++ src/components/login/Logout.vue
@@ -14,13 +14,15 @@ export default {
         const store = useStore();
 
         onMounted(() => {
-            
             if (confirm("로그아웃 하시겠습니까?")) {
                 alert("로그아웃 완료");
                 sessionStorage.removeItem("TOKEN");
                 sessionStorage.removeItem("MEMAIL");
                 store.commit('setLogged', false);
                 store.commit('ClearMemail');
+                router.push({name : 'Logout1'}); 
+            }
+            else{
                 router.push({name : 'Logout1'}); 
             }
         })
