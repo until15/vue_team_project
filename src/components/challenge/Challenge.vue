59d141b061ba93228774cbd3b71f65cf5cb18929
daheeka <alsendrha@naver.com>
2022-06-02 12:35:29 +0900

    2222
    22

--- src/components/challenge/Challenge.vue
+++ src/components/challenge/Challenge.vue
@@ -4,82 +4,66 @@
         <div>
             <h3 style="margin-top:100px;">챌린지 최신순</h3>
         </div>
-
             
-            <!-- 버튼 -->
-            <div class="center">
-                <el-button class="button-blk1" type="info" @click="handleMenu1">정보 수정</el-button>
-                <el-button class="button-blk1" type="info" @click="handleMenu3">1:1 문의</el-button>
-                <el-button class="button-blk1" type="info"   @click="handleMenu2">회원 탈퇴</el-button>
-            </div>
-            
-        <div v-for="(tmp) in state.items" :key="tmp"  align="center">
-                <el-col :span="8" :gutter="20" v-for="(j) in 1" :key="j" >
-            <el-card :body-style="{ padding: '0px' }" class="c-m" style="width:350px;height:350px">
-          <div>
-          </div>
-                <img
-                    :src="tmp.imgurl"
-                    class="image"/>
-                <div style="padding: 14px">
-                    
-                    <span>
-                        <h4>{{tmp.chgtitle}}</h4><br />
-                        {{tmp.chgintro}}<br />
-                        좋아요 {{tmp.chglike}}개<br />
-                        난이도 {{tmp.chglevel}}단계<br />
-                        {{tmp.chgfee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}원
-                    </span>
-
-                    <span class="ch-mem">
-                        {{tmp.chgrate}}
-                    </span>
-
-                    <div class="bottom">
-                        <time class="time">
-                            {{tmp.chgregdate}}
-                        </time>
-                    </div>
-
-                    <div class="chg-detail">
-                        <el-link style="margin-top:20px;" @click="handleSelect(tmp.chgno)">상세 보기</el-link>
-                    </div>
-                    
-
-                </div>
-            </el-card>
-            </el-col>
+        <!-- 버튼 -->
+        <div class="center">
+            <el-button class="button-menu" type="info" @click="handleMenu1">최신순</el-button>
+            <el-button class="button-menu" type="info" @click="handleMenu2">인기순</el-button>
+            <el-button class="button-menu" type="info"   @click="handleMenu3">난이도순</el-button>
         </div>
 
-      
+        <!-- 3*3 화면 구현 테스트 -->
+        <!-- <div class="chg-card">
+            <div>
+                <div class="center">
+                    <img :src="state.imgurl" class="image" style="width:800px;height:500px;margin-top:100px" /> <br />
+                </div>
+            </div>
             
+            <div v-for="(tmp) in state.items" :key="tmp"  align="center">
+                <el-col :span="8" :gutter="20" v-for="(j) in 1" :key="j" >
+                    <el-card :body-style="{ padding: '0px' }" style="width:300px;height:350px">
 
-                <el-table :data="state.items"  style="width: 100% " >
-                    <el-table-column prop="chgno" label="번호" width="60" />
-                    <el-table-column prop="chgtitle" label="제목"  width="250" >
-                    <template #default="scope">
-                        <div @click="handlePage(scope.row.chgno)" style="cursor:pointer;">
-                            {{scope.row.chgtitle}}
-                        </div> 
-                    </template>
-                    </el-table-column>
-                    <el-table-column prop="chgfee" label="참가비" width="100">
-                    <template #default="scope">
-                        <div>
-                            {{scope.row.chgfee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}
-                        </div> 
-                    </template>
-                    </el-table-column>
-                    <el-table-column prop="chgcnt" label="참가인원" width="80" />
-                    <el-table-column prop="chglike" label="좋아요" width="80" />
-                    <el-table-column prop="chglevel" label="난이도" width="80" />
-                    <el-table-column prop="chgregdate" label="작성일" width="100" />
+                        <div style="padding: 14px">
+                            <span>
+                                <h4>{{tmp.chgtitle}}</h4><br />
+                                {{tmp.chgintro}}<br />
+                                좋아요 {{tmp.chglike}}개<br />
+                                난이도 {{tmp.chglevel}}단계<br />
+                                {{tmp.chgfee.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}}원
+                            </span>
 
-                </el-table>
+                            <span class="ch-mem">
+                                {{tmp.chgrate}}
+                            </span>
+
+                            <div class="chg-detail">
+                                <el-link style="margin-top:20px;" @click="handleSelect(tmp.chgno)">상세 보기</el-link>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </div>
+        </div> -->
+
+        <!-- 테이블 -->
+        <el-table :data="state.items"  style="width: 100% " >
+            <el-table-column prop="chgtitle" label="제목"  width="250" >
+            <template #default="scope">
+                <div @click="handlePage(scope.row.chgno)" style="cursor:pointer;">
+                    {{scope.row.chgtitle}}
+                </div> 
+            </template>
+            </el-table-column>
+            <el-table-column prop="chgfee" label="참가비" width="100" />
+            <el-table-column prop="chgcnt" label="참가인원" width="80" />
+            <el-table-column prop="chglike" label="좋아요" width="80" />
+            <el-table-column prop="chglevel" label="난이도" width="80" />
+            <el-table-column prop="chgregdate" label="작성일" width="100" />
+        </el-table>
                 
-            
-                <el-pagination layout="prev, pager, next" :total="state.total" @current-change="currentChange">
-                </el-pagination>
+        <el-pagination layout="prev, pager, next" :total="state.total" @current-change="currentChange">
+        </el-pagination>
 
         <el-form :inline="true" v-if="state.items" >
             <!-- <select>
@@ -109,7 +93,7 @@
 
         </el-form>
         <div v-if="logged === true">
-            <el-button type="info" plain @click="handleChallenge">챌린지 생성</el-button>
+            <el-button class="button-write" style="margin-bottom:60px;" type="info" plain @click="handleChallenge">챌린지 생성</el-button>
         </div>
         <div v-if="logged === false">
             <el-button type="info" plain @click="handleMain">메인으로</el-button>
@@ -150,13 +134,11 @@ export default {
             console.log(response.data);
             if(response.data.status === 200) {
                 state.items = response.data.result;
-                state.images = response.data.images;
+                //state.imgurl = response.data.image;
                 state.total = response.data.total;
                 state.challenge= '';
             }
         }
-
-
 
         const handleMenu1 = () => {
             router.push({name : "Challenge"});
@@ -215,6 +197,11 @@ export default {
 </script>
 
 <style lang="css" scoped>
+    .chg-card {
+        background-color: #999;
+        
+        width: 900px;
+    }
 
     .c-m {
         margin: 3rem;
@@ -245,10 +232,10 @@ export default {
         padding: 5px;
     }
 
-    .button-blk1 {
-        background-color: #212529;
-        border: none;
-        color: rgb(255, 255, 255);
+    .button-menu {
+        background-color: #ffffff;
+        border: 2px solid #212529;
+        color: #212529;
         padding: 10px 20px;
         text-align: center;
         text-decoration: none;
@@ -256,6 +243,21 @@ export default {
         margin: 4px 2px;
         cursor: pointer;
         border-radius: 16px;
+        width: 80px;
+    }
+
+    .button-write {
+        background-color: #212529;
+        border:none;
+        color: #ffffff;
+        padding: 10px 20px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        margin: 4px 2px;
+        cursor: pointer;
+        border-radius: 16px;
+        width: 300px;
     }
 
 </style>
